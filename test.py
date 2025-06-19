4cd7a5f74e6309ce73c105da84871772288ee4cf
RAYMONDNJOROGE <raymondnjoroge20@gmail.com>
2025-06-19 21:32:22 +0300

    ytre

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
-# Replace with your credentials
-client_id = "36f9c27a476441b998215a3e7d320dc7"
-client_secret = "3c64f512eaae4daa9fffafac8ec3f871"
+# Load credentials from environment variables
+client_id = ("36f9c27a476441b998215a3e7d320dc7")
+client_secret = ("3c64f512eaae4daa9fffafac8ec3f871")
 
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
@@ -16,9 +16,6 @@ def index():
     tracks = []
     if request.method == "POST":
         query = request.form["query"]
-        results = sp.search(q=query, type="track", limit=10)
+        results = sp.search(q=query, type="track", limit=5)
         tracks = results["tracks"]["items"]
     return render_template("index.html", tracks=tracks)
-
-if __name__ == "__main__":
-    app.run(debug=True)
